2bc9d1e84ba3e7ace59c74b5489756a9cd9dbca5
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-02-11 09:24:48 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+function test(args){
+  let n = (args.length - 1) / 2;
+  let leftSum = 0;
+  let rightSum = 0;
+
+  for(var i = 1; i <= n; i++){
+    let currentNum = args[i];
+    leftSum += currentNum;
+  }
+  for(var j = n + 1; j < args.length; j++){
+    let currentNums = args[j];
+    rightSum += currentNums;
+  }
+  
+  if(leftSum === rightSum){
+    console.log('Yes! Sum = ' + leftSum);
+  } else{
+    console.log("No! Diff = " + Math.abs(leftSum - rightSum));
+  }
+  
+}
+test([4,55,55,100,15]);
